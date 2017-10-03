dea9cf278d6f89e68d62eeba5fd17ea62a92c26d
Shiva <shivavdc01@gmail.com>
2017-10-03 17:13:12 +0530

    test2

--- src/main/java/org/mitre/web/HomeController.java
+++ src/main/java/org/mitre/web/HomeController.java
@@ -29,6 +29,7 @@ import org.mitre.openid.connect.client.OIDCAuthenticationFilter;
 
 import org.mitre.openid.connect.client.SubjectIssuerGrantedAuthority;
 import org.slf4j.Logger;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;

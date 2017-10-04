52d608ca71e18ed2593621c804c5700244e785b3
Shiva <shivavdc01@gmail.com>
2017-10-04 16:51:14 +0530

    poll scm

--- src/main/java/org/mitre/web/HomeController.java
+++ src/main/java/org/mitre/web/HomeController.java
@@ -32,6 +32,7 @@ import org.mitre.openid.connect.client.SubjectIssuerGrantedAuthority;
 import org.slf4j.Logger;
 
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
